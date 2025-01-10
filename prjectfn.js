be6162213d6eb5c884df8effbd7843bff95e5fda
Arun-clover <arunkumarkarna.2104@gmail.com>
2025-01-10 11:02:26 +0530

    commit it

--- prjectfn.js
+++ prjectfn.js
@@ -1,5 +1,18 @@
-var one=document.getElementById("one");
-var two=document.getElementById("two");
-function numone(){
-    var value1=Number(one.value);   
+let display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        alert("Invalid expression");
+        clearDisplay();
+    }
 }
